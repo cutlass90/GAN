7355023dd54e063ba8b85f1094d31c3cbabf6b26
Ivan Mazepa <mazepa_4@yahoo.com>
2017-06-25 21:23:16 +0300

    Base commit. It works so-so

--- GAN.py
+++ GAN.py
@@ -11,10 +11,11 @@ from model_abstract import Model
 
 class GAN(Model):
 
-    def __init__(self, do_train, input_dim, z_dim, batch_size, scope):
+    def __init__(self, do_train, input_dim, n_classes, z_dim, batch_size, scope):
 
         self.do_train = do_train
         self.input_dim = input_dim
+        self.n_classes = n_classes
         self.z_dim = z_dim
         self.batch_size = batch_size
         self.scope = scope
@@ -25,8 +26,10 @@ class GAN(Model):
         with tf.variable_scope(scope):
             self.create_graph()
         if do_train:
-            self.discriminator_cost = self.get_discriminator_cost(self.logits)
-            self.generator_cost = self.get_generator_cost(self.logits)
+            self.discriminator_cost = self.get_discriminator_cost(self.targets,
+                self.logits_class_r, self.logits_critic_r, self.logits_critic_f)
+            self.generator_cost = self.get_generator_cost(self.targets_f,
+                self.logits_critic_f, self.logits_fake)
             
             self.train_disc, self.train_gen = self.create_optimizer_graph(
                 self.discriminator_cost, self.generator_cost)
@@ -44,15 +47,20 @@ class GAN(Model):
     def create_graph(self):
         print('Creat graph')
         self.inputs,\
+        self.targets,\
         self.keep_prob,\
         self.weight_decay,\
         self.learn_rate = self.input_graph()
         
         z = tf.random_normal([self.batch_size//2, self.z_dim])
-        self.fake_x = self.generator(data_dim=self.input_dim, z=z)
+        self.x_fake, self.logits_fake = self.generator(data_dim=self.input_dim, z=z,
+            n_classes=self.n_classes)
 
-        x = tf.concat((self.inputs, self.fake_x), 0)
-        self.logits = self.discriminator(x) # b x 1
+        x = tf.concat((self.inputs, self.x_fake), 0)
+        self.logits_class_r,\
+        self.logits_critic_r,\
+        self.targets_f,\
+        self.logits_critic_f = self.discriminator(x, self.n_classes) # b x 1
         print('Done!')
 
 
@@ -60,14 +68,15 @@ class GAN(Model):
     def input_graph(self):
         print('\tinput_graph')
         inputs = tf.placeholder(tf.float32, shape=[self.batch_size//2, self.input_dim], name='inputs')
+        targets = tf.placeholder(tf.float32, shape=[self.batch_size//2, self.n_classes], name='targets')
         keep_prob = tf.placeholder(tf.float32, name='keep_prob')
         weight_decay = tf.placeholder(tf.float32, name='weight_decay')
         learn_rate = tf.placeholder(tf.float32, name='learn_rate')
-        return inputs, keep_prob, weight_decay, learn_rate
+        return inputs, targets, keep_prob, weight_decay, learn_rate
 
 
     # --------------------------------------------------------------------------
-    def generator(self, data_dim, z):
+    def generator(self, data_dim, z, n_classes):
         print('\tgenerator')
         with tf.variable_scope('generator'):
             fc = tf.layers.dense(inputs=z, units=data_dim, activation=None,
@@ -75,16 +84,18 @@ class GAN(Model):
             fc = tf.contrib.layers.batch_norm(inputs=fc, scale=True,
                 updates_collections=None, is_training=self.do_train)
             fc = tf.nn.elu(fc)
-            fc = tf.layers.dense(inputs=fc, units=data_dim, activation=tf.sigmoid,
+            fc = tf.layers.dense(inputs=fc, units=data_dim+n_classes, activation=None,
                 kernel_initializer=tf.contrib.layers.xavier_initializer())
+            x_fake = tf.sigmoid(fc[:, :data_dim])
+            logits_fake = fc[:, data_dim:]
 
-        images = tf.reshape(fc, [self.batch_size//2, 28, 28, 1])
-        self.gen_sum.append(tf.summary.image('generated img', images, max_outputs=12))
-        return fc
+        images = tf.reshape(x_fake, [self.batch_size//2, 28, 28, 1])
+        self.gen_sum.append(tf.summary.image('generated img', images, max_outputs=100))
+        return x_fake, logits_fake
 
 
     # --------------------------------------------------------------------------
-    def discriminator(self, x):
+    def discriminator(self, x, n_classes):
         print('\tdiscriminator')
         with tf.variable_scope('discriminator'):
             fc = tf.layers.dense(inputs=x, units=self.input_dim, activation=None,
@@ -92,36 +103,82 @@ class GAN(Model):
             fc = tf.contrib.layers.batch_norm(inputs=fc, scale=True,
                 updates_collections=None, is_training=self.do_train)
             fc = tf.nn.elu(fc)
-            fc = tf.layers.dense(inputs=fc, units=1, activation=None,
+            fc = tf.layers.dense(inputs=fc, units=n_classes + 1, activation=None,
                 kernel_initializer=tf.contrib.layers.xavier_initializer())
-        return fc
+            fc_r = fc[:self.batch_size//2, :]
+            fc_f = fc[self.batch_size//2:, :]
+            logits_class_r = fc_r[:,:n_classes]
+            logits_critic_r = fc_r[:,n_classes:n_classes+1]
+            targets_f = tf.nn.softmax(fc_f[:,:n_classes])
+            logits_critic_f = fc_f[:,n_classes:n_classes+1]
+
+        return logits_class_r, logits_critic_r, targets_f, logits_critic_f
 
 
     # --------------------------------------------------------------------------
-    def get_discriminator_cost(self, logits):
-        print('\tget_discriminator_cost')
+    def get_discriminator_cost(self, targets, logits_class_r, logits_critic_r,
+        logits_critic_f):
+        print('get_discriminator_cost')
         ones = tf.constant(value=1., shape=[self.batch_size//2,1])
         zeros = tf.constant(value=0., shape=[self.batch_size//2,1])
-        true_labels = tf.concat([ones, zeros], axis=0)
 
-        cost = tf.nn.sigmoid_cross_entropy_with_logits(labels=true_labels,
-            logits=logits)
+        loss_class_r = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(
+            labels=targets, logits=logits_class_r))  # b/2 x 10
+        loss_critic_r = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(
+            labels=ones, logits=logits_critic_r)) # b/2 x 1
+        loss_critic_f = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(
+            labels=zeros, logits=logits_critic_f)) # b/2 x 1
+        cost = loss_class_r + loss_critic_r + loss_critic_f
+        with tf.name_scope('discriminator'):
+            self.disc_sum.append(tf.summary.scalar('loss_class_r', loss_class_r))
+            self.disc_sum.append(tf.summary.scalar('loss_critic_r', loss_critic_r))
+            self.disc_sum.append(tf.summary.scalar('loss_critic_f', loss_critic_f))
 
-        cost = tf.reduce_mean(cost)
-        self.disc_sum.append(tf.summary.scalar('discriminator cost', cost))
+            pred = tf.reduce_max(logits_class_r, axis=1)
+            pred = tf.cast(tf.equal(logits_class_r, tf.expand_dims(pred, 1)), tf.float32)
+            for i in range(self.n_classes):
+                y = targets[:,i]
+                y_ = pred[:,i]
+                tp = tf.reduce_sum(y*y_)
+                tn = tf.reduce_sum((1-y)*(1-y_))
+                fp = tf.reduce_sum((1-y)*y_)
+                fn = tf.reduce_sum(y*(1-y_))
+                pr = tp/(tp+fp+1e-5)
+                re = tp/(tp+fn+1e-5)
+                f1 = 2*pr*re/(pr+re+1e-5)
+                with tf.name_scope('Class_{}'.format(i)):
+                    self.disc_sum.append(tf.summary.scalar('Class {} precision'.format(i), pr))
+                    self.disc_sum.append(tf.summary.scalar('Class {} recall'.format(i), re))
+                    self.disc_sum.append(tf.summary.scalar('Class {} f1 score'.format(i), f1))
         return cost
 
 
     # --------------------------------------------------------------------------
-    def get_generator_cost(self, logits):
-        print('\tget_generator_cost')
-        true_labels = tf.constant(value=1., shape=[self.batch_size//2,1])
+    def get_generator_cost(self, targets_f, logits_critic_f, logits_fake):
+        print('get_generator_cost')
+        ones = tf.constant(value=1., shape=[self.batch_size//2,1])
 
-        cost = tf.nn.sigmoid_cross_entropy_with_logits(labels=true_labels,
-            logits=logits[self.batch_size//2:,:])
+        loss_class_f = 30*tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(
+            labels=targets_f, logits=logits_fake))
+        loss_critic_f = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(
+            labels=ones, logits=logits_critic_f))
+        # class_distrib = tf.nn.softmax(tf.reduce_sum(logits_fake, 0))
+        class_distrib = tf.reduce_mean(tf.nn.softmax(logits_fake),0)
+        # class_distrib = tf.Print(class_distrib, [class_distrib], summarize=100)
+        labels=tf.constant(value=1./self.n_classes, shape=[self.n_classes],
+                dtype=tf.float32)
+        loss_distrib = -20*tf.reduce_sum(labels*tf.log(class_distrib+1e-6))
+            
+        cost = loss_class_f + loss_critic_f + loss_distrib
 
-        cost = tf.reduce_mean(cost)
-        self.gen_sum.append(tf.summary.scalar('generator cost', cost))
+        class_balance_f = tf.argmax(logits_fake, axis=1)
+        class_balance_r = tf.argmax(targets_f, axis=1)
+        self.gen_sum.append(tf.summary.histogram('class_balance_fake', class_balance_f))
+        self.gen_sum.append(tf.summary.histogram('class_balance_real', class_balance_r))
+        with tf.name_scope('generator'):
+            self.gen_sum.append(tf.summary.scalar('loss_class_f', loss_class_f))
+            self.gen_sum.append(tf.summary.scalar('loss_critic_f', loss_critic_f))
+            self.gen_sum.append(tf.summary.scalar('loss_distrib', loss_distrib))
         return cost
 
 
@@ -146,9 +203,6 @@ class GAN(Model):
                 for t,n in gen_list_grad if t is not None])
             self.gen_sum.append(tf.summary.scalar('gen_grad', gen_grad))
             train_gen = gen_optimizer.apply_gradients(gen_list_grad)
-
-
-
         return train_disc, train_gen
 
 
@@ -163,17 +217,20 @@ class GAN(Model):
                 n_iter, current_iter)
             batch = data_loader.next_batch(self.batch_size//2)
             feedDict = {self.inputs : batch[0],
+                        self.targets : batch[1],
                         self.keep_prob : keep_prob,
                         self.weight_decay : weight_decay,
                         self.learn_rate : learn_rate}
                         
             _, summary = self.sess.run([self.train_disc, self.disc_merge],
                 feed_dict=feedDict)
-            self.train_writer.add_summary(summary, current_iter)
+            if current_iter%50 == 0:
+                self.train_writer.add_summary(summary, current_iter)
 
             _, summary = self.sess.run([self.train_gen, self.gen_merge],
                 feed_dict=feedDict)
-            self.train_writer.add_summary(summary, current_iter)
+            if current_iter%50 == 0:
+                self.train_writer.add_summary(summary, current_iter)
 
             if (current_iter+1) % save_model_every_n_iter == 0:
                 self.save_model(path=path_to_model, sess=self.sess, step=current_iter+1)
--- run_GAN.py
+++ run_GAN.py
@@ -13,7 +13,9 @@ os.makedirs('summary/', exist_ok=True)
 mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 
 
-gan = GAN(do_train=True, input_dim=784, z_dim=20, batch_size=256, scope='GAN')
+gan = GAN(do_train=True, input_dim=784, n_classes=10, z_dim=20, batch_size=256,
+	scope='GAN')
+# gan.load_model(path='models/gan-100000', sess=gan.sess)
 gan.train_(data_loader=mnist.train, keep_prob=1, weight_decay=0,
-    learn_rate_start=0.001, learn_rate_end=0.0001,  n_iter=10000,
-    save_model_every_n_iter=99999, path_to_model='models/gan')
+    learn_rate_start=0.001, learn_rate_end=0.0001,  n_iter=100000,
+    save_model_every_n_iter=10000, path_to_model='models/gan')
