a9f418b65c9c37dffbf286da1402d73f1b91be5d
nazars <cutlass90@mail.ru>
2017-06-26 12:42:12 +0300

    add structure to generator and discriminator, add saving sampling each 1000 iterations

--- GAN.py
+++ GAN.py
@@ -8,6 +8,7 @@ import numpy as np
 from tqdm import tqdm
 
 from model_abstract import Model
+from plot import sample as plot_samples
 
 class GAN(Model):
 
@@ -46,13 +47,14 @@ class GAN(Model):
         self.inputs,\
         self.keep_prob,\
         self.weight_decay,\
-        self.learn_rate = self.input_graph()
+        self.learn_rate,\
+        self.is_training = self.input_graph()
         
         z = tf.random_normal([self.batch_size//2, self.z_dim])
-        self.fake_x = self.generator(data_dim=self.input_dim, z=z)
+        self.x_fake = self.generator(z=z, structure=[256, 256, self.input_dim])
 
-        x = tf.concat((self.inputs, self.fake_x), 0)
-        self.logits = self.discriminator(x) # b x 1
+        x = tf.concat((self.inputs, self.x_fake), 0)
+        self.logits = self.discriminator(x, structure=[256, 256, 1]) # b x 1
         print('Done!')
 
 
@@ -63,38 +65,40 @@ class GAN(Model):
         keep_prob = tf.placeholder(tf.float32, name='keep_prob')
         weight_decay = tf.placeholder(tf.float32, name='weight_decay')
         learn_rate = tf.placeholder(tf.float32, name='learn_rate')
-        return inputs, keep_prob, weight_decay, learn_rate
-
+        is_training = tf.placeholder(tf.bool, name='is_training')
+        return inputs, keep_prob, weight_decay, learn_rate, is_training
 
     # --------------------------------------------------------------------------
-    def generator(self, data_dim, z):
+    def generator(self, z, structure):
         print('\tgenerator')
         with tf.variable_scope('generator'):
-            fc = tf.layers.dense(inputs=z, units=data_dim, activation=None,
-                kernel_initializer=tf.contrib.layers.xavier_initializer())
-            fc = tf.contrib.layers.batch_norm(inputs=fc, scale=True,
-                updates_collections=None, is_training=self.do_train)
-            fc = tf.nn.elu(fc)
-            fc = tf.layers.dense(inputs=fc, units=data_dim, activation=tf.sigmoid,
+            for layer in structure[:-1]:
+                z = tf.layers.dense(inputs=z, units=layer, activation=None,
+                    kernel_initializer=tf.contrib.layers.xavier_initializer())
+                z = tf.contrib.layers.batch_norm(inputs=z, scale=True,
+                    updates_collections=None, is_training=self.is_training)
+                z = tf.nn.elu(z)
+            z = tf.layers.dense(inputs=z, units=self.input_dim, activation=tf.sigmoid,
                 kernel_initializer=tf.contrib.layers.xavier_initializer())
 
-        images = tf.reshape(fc, [self.batch_size//2, 28, 28, 1])
-        self.gen_sum.append(tf.summary.image('generated img', images, max_outputs=12))
-        return fc
+        images = tf.reshape(z, [self.batch_size//2, 28, 28, 1])
+        self.gen_sum.append(tf.summary.image('generated img', images, max_outputs=100))
+        return z
 
 
     # --------------------------------------------------------------------------
-    def discriminator(self, x):
+    def discriminator(self, x, structure):
         print('\tdiscriminator')
         with tf.variable_scope('discriminator'):
-            fc = tf.layers.dense(inputs=x, units=self.input_dim, activation=None,
+            for layer in structure[:-1]:
+                x = tf.layers.dense(inputs=x, units=layer, activation=None,
+                    kernel_initializer=tf.contrib.layers.xavier_initializer())
+                x = tf.contrib.layers.batch_norm(inputs=x, scale=True,
+                    updates_collections=None, is_training=self.is_training)
+                x = tf.nn.elu(x)
+            x = tf.layers.dense(inputs=x, units=structure[-1], activation=None,
                 kernel_initializer=tf.contrib.layers.xavier_initializer())
-            fc = tf.contrib.layers.batch_norm(inputs=fc, scale=True,
-                updates_collections=None, is_training=self.do_train)
-            fc = tf.nn.elu(fc)
-            fc = tf.layers.dense(inputs=fc, units=1, activation=None,
-                kernel_initializer=tf.contrib.layers.xavier_initializer())
-        return fc
+        return x
 
 
     # --------------------------------------------------------------------------
@@ -165,7 +169,8 @@ class GAN(Model):
             feedDict = {self.inputs : batch[0],
                         self.keep_prob : keep_prob,
                         self.weight_decay : weight_decay,
-                        self.learn_rate : learn_rate}
+                        self.learn_rate : learn_rate,
+                        self.is_training : True}
                         
             _, summary = self.sess.run([self.train_disc, self.disc_merge],
                 feed_dict=feedDict)
@@ -175,9 +180,19 @@ class GAN(Model):
                 feed_dict=feedDict)
             self.train_writer.add_summary(summary, current_iter)
 
+            if current_iter%1000 == 0:
+                samples = self.sample()
+                plot_samples(samples, current_iter)
+
             if (current_iter+1) % save_model_every_n_iter == 0:
                 self.save_model(path=path_to_model, sess=self.sess, step=current_iter+1)
 
         self.save_model(path=path_to_model, sess=self.sess, step=current_iter+1)
         print('\nTrain finished!')
         print("Training time --- %s seconds ---" % (time.time() - start_time))
+
+
+    #---------------------------------------------------------------------------
+    def sample(self):
+        samples = self.sess.run(self.x_fake, {self.is_training:False})
+        return samples[:100,...]
