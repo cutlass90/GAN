28ad8336e565b191a2e41dd6946e9c2ca4736ad1
Nazar <cutlass90@mail.ru>
2017-07-25 15:58:46 +0300

    rewrite GAN for semi

--- run_GAN.py
+++ run_GAN.py
@@ -11,7 +11,7 @@ from GAN import GAN
 
 mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 gan = GAN(do_train=True, input_dim=784, n_classes=10, z_dim=20, scope='GAN')
-gan.load_model(path='models/', sess=gan.sess)
-gan.train_(data_loader=mnist, batch_size=128, keep_prob=1, weight_decay=0,
+# gan.load_model(path='models/', sess=gan.sess)
+gan.train_model(data_loader=mnist, batch_size=128, keep_prob=1, weight_decay=0,
     learn_rate_start=0.001, learn_rate_end=0.0001,  n_iter=100000,
     save_model_every_n_iter=10000, path_to_model='models/gan')
